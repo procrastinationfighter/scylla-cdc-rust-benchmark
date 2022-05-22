ed335a651a5619fe23babcf7e9cdc05e47d04383
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2022-05-22 15:20:32 +0200

    Finish java benchmark

--- java-reader/cdc-rust-benchmark/src/main/java/com/scylladb/cdc/rustbenchmark/Benchmark.java
+++ java-reader/cdc-rust-benchmark/src/main/java/com/scylladb/cdc/rustbenchmark/Benchmark.java
@@ -3,66 +3,58 @@ package com.scylladb.cdc.rustbenchmark;
 import com.scylladb.cdc.lib.CDCConsumer;
 import com.scylladb.cdc.lib.RawChangeConsumerProvider;
 import com.scylladb.cdc.model.TableName;
-import com.scylladb.cdc.model.worker.RawChangeConsumer;
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 
-/**
- * Hello world!
- *
- */
 public class Benchmark {
     public static void main(String[] args) {
+        Namespace parsedArguments = parseArguments(args);
+        String source = parsedArguments.getString("source");
+        String keyspace = parsedArguments.getString("keyspace"), table = parsedArguments.getString("table");
+        int rowCount = Integer.parseInt(parsedArguments.getString("rowcount"));
+        CountDownLatch terminationLatch = new CountDownLatch(rowCount);
 
-        // Build a provider of consumers. The CDCConsumer instance
-        // can be run in multi-thread setting and a separate
-        // RawChangeConsumer is used by each thread.
         RawChangeConsumerProvider changeConsumerProvider = threadId -> {
-            // Build a consumer of changes. You should provide
-            // a class that implements the RawChangeConsumer
-            // interface.
-            //
-            // Here, we use a lambda for simplicity.
-            //
-            // The consume() method of RawChangeConsumer returns
-            // a CompletableFuture, so your code can perform
-            // some I/O responding to the change.
-            //
-            // The RawChange name alludes to the fact that
-            // changes represented by this class correspond
-            // 1:1 to rows in *_scylla_cdc_log table.
-            return change -> CompletableFuture.completedFuture(null);
+            return change -> {
+                terminationLatch.countDown();
+                return CompletableFuture.completedFuture(null);
+            };
         };
 
-        // Build a CDCConsumer, which is single-threaded
-        // (workersCount(1)), reads changes
-        // from [keyspace].[table] and passes them
-        // to consumers created by changeConsumerProvider.
         try (CDCConsumer consumer = CDCConsumer.builder()
-                .addContactPoint("172.17.0.2:9042")
-                .addTable(new TableName("ks", "t1"))
+                .addContactPoint(source)
+                .addTable(new TableName(keyspace, table))
                 .withConsumerProvider(changeConsumerProvider)
-                .withWorkersCount(1)
                 .build()) {
-
-            // Start a consumer. You can stop it by using .stop() method
-            // or it can be automatically stopped when created in a
-            // try-with-resources (as shown above).
             consumer.start();
 
-            // The consumer is started in background threads.
-            // It is consuming the CDC log and providing read changes
-            // to the consumers.
-
-            // Wait for SIGINT:
-            CountDownLatch terminationLatch = new CountDownLatch(1);
-            Signal.handle(new Signal("INT"), signal -> terminationLatch.countDown());
             terminationLatch.await();
         } catch (InterruptedException ex) {
-            System.err.println("Exception occurred while running the Printer: "
+            System.err.println("Exception occurred while running the Benchmark: "
                     + ex.getMessage());
         }
+    }
 
-        // The CDCConsumer is gracefully stopped after try-with-resources.
+    private static Namespace parseArguments(String[] args) {
+        ArgumentParser parser = ArgumentParsers.newFor("./cdc-rust-benchmark").build().defaultHelp(true);
+        parser.addArgument("-k", "--keyspace").required(true).help("Keyspace name");
+        parser.addArgument("-t", "--table").required(true).help("Table name");
+        parser.addArgument("-s", "--source").required(true)
+                .setDefault("127.0.0.1").help("Address of a node in source cluster");
+        parser.addArgument("-c", "--rowcount").required(true).help("Number of rows to read.");
+
+        try {
+            return parser.parseArgs(args);
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(-1);
+            return null;
+        }
     }
 }
