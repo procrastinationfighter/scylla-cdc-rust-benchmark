96b74a32198adeba14d782094294df05664dd564
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2022-05-26 00:34:31 +0200

    Finish the benchmark script

--- run_benchmark.py
+++ run_benchmark.py
@@ -1,9 +1,11 @@
 import argparse
+from cassandra.cluster import Cluster
 import subprocess
+import time
 
 # TODO: Configure parameters: safety interval etc.
 rust_binary = "rust-reader/target/release/scylla-cdc-rust-benchmark"
-java_binary = "java-reader/cdc-rust-benchmark"
+java_binary = "java-reader/cdc-rust-benchmark/cdc-rust-benchmark"
 scylla_bench_binary = "scylla-bench/scylla-bench"
 keyspace = "scylla_bench"
 table = "test"
@@ -11,61 +13,87 @@ table = "test"
 time_command = "/usr/bin/time -v"
 
 
-def write_to_database(source: str, partition_count: int, clustering_row_count: int):
+def prepare_database(source: str, partition_count: int, clustering_row_count: int, max_rate: int):
+    cluster = Cluster([source])
+    session = cluster.connect()
+    session.execute(f"CREATE KEYSPACE IF NOT EXISTS {keyspace} "
+                    f"WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': 1}}")
+    session.execute(f"CREATE TABLE IF NOT EXISTS {keyspace}.{table} "
+                    f"(pk bigint, ck bigint, v blob, primary key (pk, ck))"
+                    f"WITH cdc = {{'enabled': 'true'}} AND compression = {{ }}")
+
+    print("Writing the test data to the cluster...")
     command = [scylla_bench_binary,
                "-nodes", source,
                "-mode", "write",
                "-workload", "sequential",
                "-partition-count", f"{partition_count}",
-               "-clustering-row-count", f"{clustering_row_count}"]
+               "-clustering-row-count", f"{clustering_row_count}",
+               "-max-rate", f"{max_rate}"]
 
     subprocess.run(command, stdout=subprocess.DEVNULL)
 
+    print("Data written!")
 
-def run_rust(source: str, rows_count: int):
-    # TODO: Add correct timestamp
+
+def run_rust(source: str, rows_count: int, window_size: int):
     command = ["/usr/bin/time", "-v",
                rust_binary,
                "--keyspace", keyspace,
                "--table", table,
                "--hostname", f"{source}:9042",
-               "--start-timestamp", "1970-01-01 10:00:00",
-               "--rows_count", f"{rows_count}"]
+               "--rows-count", f"{rows_count}",
+               "--window-size", f"{window_size}.0"]
 
-    print("RUNNING THE BENCHMARK FOR SCYLLA-CDC-RUST:")
-    subprocess.run(command, stdout=subprocess.STDOUT)
-    print("\n\n\n")
+    print("Running the benchmark for scylla-cdc-rust.")
+    with open(f"rust_{window_size}.txt", "w") as output_file:
+        subprocess.run(command, stdout=output_file, stderr=output_file)
 
 
-def run_java(source: str, rows_count: int):
+def run_java(source: str, rows_count: int, window_size: int):
     command = ["/usr/bin/time", "-v",
                java_binary,
                "-k", keyspace,
                "-t", table,
                "-s", source,
-               "-c", f"{rows_count}"]
+               "-c", f"{rows_count}",
+               "-w", f"{window_size * 1000}"]
 
-    print("RUNNING THE BENCHMARK FOR SCYLLA-CDC-JAVA:")
-    subprocess.run(command, stdout=subprocess.STDOUT)
-    print("\n\n\n")
+    print("Running the benchmark for scylla-cdc-java.")
+    with open(f"java_{window_size}.txt", "w") as output_file:
+        subprocess.run(command, stdout=output_file, stderr=output_file)
+
+
+def run_tests(source: str, rows_count: int, window_size: int):
+    print(f"Running the benchmark with window size equal to {window_size} seconds.")
+    run_rust(source, rows_count, window_size)
+    run_java(source, rows_count, window_size)
+    print(f"The benchmark for window size {window_size} has finished!")
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--source", type=str)
-    parser.add_argument("--partition_count", default=100, type=int)
-    parser.add_argument("--clustering_row_count", default=100, type=int)
+    parser.add_argument("--partition_count", default=10000, type=int)
+    parser.add_argument("--clustering_row_count", default=1000, type=int)
+    parser.add_argument("--max_rate", default=20000, type=int)
 
     args = parser.parse_args()
     source = args.source
 
-    # TODO: Create the table
+    prepare_database(source, args.partition_count, args.clustering_row_count, args.max_rate)
 
-    write_to_database(source, args.partition_count, args.clustering_row_count)
+    # After creating the database, sleep for 60 seconds so that the window size doesn't ruin the benchmark.
+    print("Waiting...")
+    time.sleep(60.0)
 
     rows_count = args.partition_count * args.clustering_row_count
-    run_rust(source, rows_count)
-    run_java(source, rows_count)
+
+    print("Starting the benchmark.")
+
+    run_tests(source, rows_count, 15)
+    run_tests(source, rows_count, 30)
+    run_tests(source, rows_count, 60)
 
 
 main()
